8454af58b4545e94dd5c17ee18c21975b2b0d96c
joaopguerra <joaopaulomg_@hotmail.com>
2025-05-08 14:27:56 -0300

    Method to calculate average of sleeps by user created

--- src/main/java/com.noom.interview.fullstack.sleep/repositories/SleepRepository.java
+++ src/main/java/com.noom.interview.fullstack.sleep/repositories/SleepRepository.java
@@ -4,6 +4,7 @@ import com.noom.interview.fullstack.sleep.domain.Sleep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +14,7 @@ public interface SleepRepository extends JpaRepository<Sleep, UUID> {
     List<Sleep> findByUserId(UUID userId);
 
     Optional<Sleep> findByIdAndIsDeleted(UUID id, boolean isDeleted);
+
+    List<Sleep> findByUserIdAndSleepDateBetweenAndIsDeleted(UUID userId, LocalDateTime startDate, LocalDateTime endDate,
+                                                            boolean isDeleted);
 }
